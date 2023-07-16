d906eb1e3754b7fc977b563b50886c0244a7ef05
PRADUMANSHARMA-180891 <praduman.cuh@gmail.com>
2023-07-16 22:33:44 +0530

    first

---
+++ routes/gallery.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const Gallery = require('../models/Gallery');
+
+// Gallery management routes
+router.get('/galleries/profile', async (req, res) => {
+  try {
+    const galleries = await Gallery.find().sort({ createdAt: 'desc' });
+    res.render('galleries', { galleries });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/galleries/new', (req, res) => {
+  res.render('index');
+});
+
+router.post('/galleries/post', async (req, res) => {
+  try {
+    const { name, images } = req.body;
+    const gallery = new Gallery({ name, images });
+    await gallery.save();
+    // res.status(200).send("save")
+    res.redirect('/g/galleries/profile');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+module.exports = router;
